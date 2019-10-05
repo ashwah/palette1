40d003651bbc74eb46ab2cc8b057790dfe8dacfc
Ashley George <ashley@microserve.io>
2019-10-05 23:34:16 +0100

    Making it work Async

--- index.js
+++ index.js
@@ -1,37 +1,62 @@
 const express = require('express');
 const app = express();
 
+const kmeans = require('node-kmeans');
+const getPixels = require("get-pixels");
+
 //var accessToken = '23612221.3fcb46b.348431486f3a4fb85081d5242db9ca1c';
 //var InstagramAPI = require('instagram-api');
 //var instagramAPI = new InstagramAPI(accessToken);
 
-var centroids = [];
-var markup = '';
+const img = "img2.jpg";
 
 app.get('/', function (req, res) {
-  for (var centroid in centroids) {
+  doEverything(img).then(function(centroids) {
+    var markup = '';
+    var r, g, b;
+    for (var centroid in centroids) {
 
-    var r = centroids[centroid][0];
-    var g = centroids[centroid][1];
-    var b = centroids[centroid][2];
+      r = centroids[centroid][0];
+      g = centroids[centroid][1];
+      b = centroids[centroid][2];
 
-    markup += '<div style="background-color:rgb(' + r + ', ' + g + ', ' + b + ' )">HELLO</div></br>';
-
-  }
-  res.send(markup);
+      markup += '<div style="background-color:rgb(' + r + ', ' + g + ', ' + b + ' )">HELLO</div></br>';
+    }
+    res.send(markup);
+  });
 })
 
 app.listen(3000);
 
-var getPixels = require("get-pixels");
+/**
+ * Promisified getPixels function.
+ *
+ * Attempts to load an image from the path.
+ *
+ * @param img
+ * @returns {Promise<unknown>}
+ */
+function getImagePixels(img) {
+  return new Promise(function (resolve) {
+    getPixels(img, function(err, pixels) {
+      if(err) {
+        console.log("Bad image path")
+        return
+      }
+      resolve(pixels);
+    })
+  })
+}
 
-getPixels("img2.jpg", function(err, pixels) {
-  if(err) {
-    console.log("Bad image path")
-    return
-  }
-  console.log(pixels.shape);
-
+/**
+ * Preprocess the image pixels.
+ *
+ * Filter based on some HSL parmeters.
+ *
+ * @param pixels
+ * @returns {Promise<[]>}
+ */
+async function preprocessPixels(pixels) {
   var out = [];
   for (var i = 0; i < pixels.shape[0]; i++) {
     for (var j = 0; j < pixels.shape[1]; j++) {
@@ -39,28 +64,60 @@ getPixels("img2.jpg", function(err, pixels) {
       var g = pixels.get(i,j,1);
       var b = pixels.get(i,j,2);
       hsl = rgbToHsl(r,g,b);
-      console.log(hsl);
       if (hsl[1] > 0.3 && hsl[2] < 0.8 && hsl[2] > 0.2) {
         out.push([r, g, b]);
       }
     }
   }
+  return Promise.resolve(out);
+}
 
-  const kmeans = require('node-kmeans');
-  kmeans.clusterize(out, {k: 10}, (err,res) => {
-    console.log('clustering');
-    if (err) console.error(err);
-    else
+/**
+ * Apply the clustering k-means algorithm.
+ *
+ * @param out
+ * @returns {Promise<unknown>}
+ */
+function cluster(out) {
+  return new Promise(function (resolve) {
+    let centroids = [];
+    kmeans.clusterize(out, {k: 10}, (err,res) => {
+      console.log('clustering');
+      if (err) console.error(err);
+      else
       //console.log('%o',res);
 
-      for (var result in res) {
-        centroids.push(res[result].centroid);
-      }
+        for (var result in res) {
+          //console.log('xxxxx' + res[result].centroid);
+          centroids.push(res[result].centroid);
+        }
+        resolve(centroids);
+    });
+  })
+}
 
-  });
-})
+/**
+ * Combine the proceeding async functions.
+ *
+ * @param img
+ * @returns {Promise<unknown>}
+ */
+async function doEverything(img) {
+  var pixels = await getImagePixels(img);
+  var output = await preprocessPixels(pixels);
+  var centroids = await cluster(output);
 
+  return Promise.resolve(centroids);
+}
 
+/**
+ * Convert RGB to HSL.
+ *
+ * @param r
+ * @param g
+ * @param b
+ * @returns {[number, number, number]}
+ */
 function rgbToHsl(r, g, b){
   r /= 255, g /= 255, b /= 255;
   var max = Math.max(r, g, b), min = Math.min(r, g, b);
